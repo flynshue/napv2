9edef43858cbd0dcc5da76466887525b12cbcce6
Felicia Lyn-Shue <flynshue@gmail.com>
2021-06-21 18:03:23 -0400

    fixing more token auth

--- auth_test.go
+++ auth_test.go
@@ -14,7 +14,7 @@ func TestAuthBasic(t *testing.T) {
 func TestAuthToken(t *testing.T) {
 	auth := AuthToken{Token: "56451208-d294-11eb-8bd0-c8f75077546c"}
 	want := "token 56451208-d294-11eb-8bd0-c8f75077546c"
-	got := auth.AuthorizationToken()
+	got := auth.AuthorizationHeader()
 	if got != want {
 		t.Fatalf("error AuthorizationHeader(); got %s, want %s\n", got, want)
 	}
